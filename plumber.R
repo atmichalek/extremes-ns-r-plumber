362a25a38d4a3d935422562af93593c5b2235282
atmichalek <97183657+atmichalek@users.noreply.github.com>
2022-04-23 16:54:57 -0500

    changes to gamlss code

--- plumber.R
+++ plumber.R
@@ -9,25 +9,42 @@ function(req, res) {
   res
 }
 
-#* Echo back the input
-#* @param msg The message to echo
-#* @get /echo
-function(msg=""){
-  list(msg = paste0("The message is: '", msg, "'"))
-}
+#* Return the exceedance discharge of simulations
+#* param input json of data
+#* @get /nsfit
+function(input){
+  library(extRemes)
+  library(jsonlite)
+  
+  data<- fromJSON(input)
+  print(data)
+  n<-length(data)
+  yrs<-data[1:(n/2)]
+  flow<-data[(n/2+1):n]
+  data <- data.frame(cbind(yrs,flow))
+  colnames(data) <- c("yrs","flow")
+  
+  GEV.NS <- extRemes::fevd(flow, data = data, location = ~yrs, type="GEV")
+  
+  # Non-stationary fit GEV distribution
+  par_loc0.GEV.NS <- GEV.NS$results$par[[1]] #loc 1 = mu0
+  par_loc1.GEV.NS <- GEV.NS$results$par[[2]] #loc 1 = mu0
+  par_scale.GEV.NS <- GEV.NS$results$par[[3]] #scale
+  par_shape.GEV.NS <- GEV.NS$results$par[[4]] # shape
+  
+  
+  ## quantiles over time 
+  ACE <- c(0.9, 0.99) # 10 yrs and 100 yrs 
+  quant.yrs <- matrix(0,length(yrs),length(ACE))
+  
+  for(i in 1:length(ACE)){
+    shape.1 <- par_shape.GEV.NS
+    scale.1 <- par_scale.GEV.NS
+    for(j in 1:length(yrs)){
+      loc.1 <- par_loc0.GEV.NS + par_loc1.GEV.NS *j 
+      quant.yrs[j,i] <- extRemes::qevd(ACE[i],loc = loc.1, scale = scale.1, shape = shape.1, type = "GEV") 
+    }      
+  }
 
-#* Plot a histogram
-#* @png
-#* @get /plot
-function(){
-  rand <- rnorm(100)
-  hist(rand)
-}
-
-#* Return the sum of two numbers
-#* @param a The first number to add
-#* @param b The second number to add
-#* @post /sum
-function(a, b){
-  as.numeric(a) + as.numeric(b)
+  return(quant.yrs)
 }
