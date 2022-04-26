a8b09f0807ef4f92ce0aea4f530242ad7feeb623
atmichalek <97183657+atmichalek@users.noreply.github.com>
2022-04-26 14:15:29 -0500

    updated code

--- plumber.R
+++ plumber.R
@@ -2,6 +2,11 @@
 #* @apiTitle extRemes NSFIT
 #* @apiDescription An API to send a JSON array of year and data value to fit the extRemes distributions of Gumbel, GEV, and exponential varying on time. Gumbel and GEV sets the location parameter varying linearly on time. Exponetial sets the scale parameter varying on time. The output returned is the following per year: years, probability quantiles, AIC, and the distribution parameters "mu0","mu1","sigma0","sigma1","shape".   
 #* Return the exceedance discharge of simulations
+#* @filter cors
+cors <- function(res) {
+  res$setHeader("Access-Control-Allow-Origin", "*")
+  plumber::forward()
+}
 #* param input json of data
 #* param model which distribution
 #* @get /nsfit
@@ -13,18 +18,20 @@ function(input,model){
   data<- fromJSON(input)
   yrs<-data[,1]
   flow<-data[,2]
+  print(flow)
   data <- data.frame(cbind(yrs,flow))
   colnames(data) <- c("yrs","flow")
- 
+  x<-seq(from=1,to=length(yrs),by=1)
+  data$yrs<-x
+
   #Primary File Paramaters -------
   ACE <- c(0.5,0.8,0.9,0.96,0.99,0.996) # 2, 5, 10, 25, 50, 100 and 250 yrs
   quant.yrs <- matrix(0,length(yrs),length(ACE))
-  data <- data.frame(cbind(yrs,flow))
-  colnames(data) <- c("yrs","flow")
   
   # Distribution Fits -----------
   if(model == 1){ #Gumbel
     GEV.NS <- extRemes::fevd(flow, data = data, location = ~yrs, type="Gumbel")
+    plot(GEV.NS)
     # Non-stationary fit GEV distribution
     par_loc0.GEV.NS <- GEV.NS$results$par[[1]] #loc 1 = mu0
     par_loc1.GEV.NS <- GEV.NS$results$par[[2]] #loc 1 = mu0
@@ -39,7 +46,7 @@ function(input,model){
       }      
     }
     result <- data.frame(cbind(yrs,quant.yrs,AIC_GEV.NS,par_loc0.GEV.NS,par_loc1.GEV.NS,par_scale.GEV.NS,0,0))
-    colnames(result) <- c("yrs","q_0.5","q_0.8","q_0.9","q_0.96","q_0.99","q_0.996","AIC","mu0","mu1","sigma0","sigma1","shape")
+    colnames(result) <- c("yrs","q_05","q_08","q_09","q_096","q_099","q_0996","AIC","mu0","mu1","sigma0","sigma1","shape")
   }else if(model == 2){ #GEV 
     GEV.NS <- extRemes::fevd(flow, data = data, location = ~yrs, type="GEV")
     # Non-stationary fit GEV distribution
@@ -58,7 +65,7 @@ function(input,model){
       }
     }
     result <- data.frame(cbind(yrs,quant.yrs,AIC_GEV.NS,par_loc0.GEV.NS,par_loc1.GEV.NS,par_scale.GEV.NS,0,par_shape.GEV.NS))
-    colnames(result) <- c("yrs","q_0.5","q_0.8","q_0.9","q_0.96","q_0.99","q_0.996","AIC","mu0","mu1","sigma0","sigma1","shape")
+    colnames(result) <- c("yrs","q_05","q_08","q_09","q_096","q_099","q_0996","AIC","mu0","mu1","sigma0","sigma1","shape")
     
   }else{ #Exponential
     Exp.NS <- extRemes::fevd(flow, data = data, threshold =0, scale = ~yrs, type="Exponential")
@@ -75,7 +82,7 @@ function(input,model){
       }
     }
   result <- data.frame(cbind(yrs,quant.yrs,AIC_Exp.NS,0,0,par_scale0.Exp.NS,par_scale1.Exp.NS,0))
-  colnames(result) <- c("yrs","q_0.5","q_0.8","q_0.9","q_0.96","q_0.99","q_0.996","AIC","mu0","mu1","sigma0","sigma1","shape")
+  colnames(result) <- c("yrs","q_05","q_08","q_09","q_096","q_099","q_0996","AIC","mu0","mu1","sigma0","sigma1","shape")
   }
   ## quantiles over time 
   return(result)
